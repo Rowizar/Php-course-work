e2b72c25f43140bf40d76e3fa4e6ce325358d213
Rowizar <114011598+Rowizar@users.noreply.github.com>
2023-10-17 00:06:15 +0300

    FINAL

--- hashtag.sql
+++ hashtag.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Окт 16 2023 г., 22:36
+-- Время создания: Окт 16 2023 г., 22:39
 -- Версия сервера: 10.4.28-MariaDB
 -- Версия PHP: 8.2.4
 
@@ -39,6 +39,66 @@ CREATE TABLE `hashtag` (
 INSERT INTO `hashtag` (`id`, `name`) VALUES
 (7, 'WWW');
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sms`
+--
+
+CREATE TABLE `sms` (
+  `id` int(11) NOT NULL,
+  `hashtag_id` text DEFAULT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `sms`
+--
+
+INSERT INTO `sms` (`id`, `hashtag_id`, `description`) VALUES
+(10, NULL, 'Я молодец'),
+(11, NULL, 'Я молодец ');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sms_hashtag`
+--
+
+CREATE TABLE `sms_hashtag` (
+  `sms_id` int(11) NOT NULL,
+  `hashtag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `sms_hashtag`
+--
+
+INSERT INTO `sms_hashtag` (`sms_id`, `hashtag_id`) VALUES
+(11, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `full_name`, `login`, `email`, `password`, `avatar`) VALUES
+(2, 'Роман', 'Rowizard', 'gravi.rom@gmail.com', '7173b225e2929ee9fec996ee1985b7b1', 'uploads/1697488477');
+
 --
 -- Индексы сохранённых таблиц
 --
@@ -50,6 +110,27 @@ ALTER TABLE `hashtag`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `sms`
+--
+ALTER TABLE `sms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `sms_hashtag`
+--
+ALTER TABLE `sms_hashtag`
+  ADD PRIMARY KEY (`sms_id`,`hashtag_id`),
+  ADD KEY `hashtag_id` (`hashtag_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`login`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -58,6 +139,29 @@ ALTER TABLE `hashtag`
 --
 ALTER TABLE `hashtag`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT для таблицы `sms`
+--
+ALTER TABLE `sms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `sms_hashtag`
+--
+ALTER TABLE `sms_hashtag`
+  ADD CONSTRAINT `sms_hashtag_ibfk_1` FOREIGN KEY (`sms_id`) REFERENCES `sms` (`id`),
+  ADD CONSTRAINT `sms_hashtag_ibfk_2` FOREIGN KEY (`hashtag_id`) REFERENCES `hashtag` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
